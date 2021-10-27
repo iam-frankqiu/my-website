ba0038fb22e446fb08fba245e1facb3fb5467d08
FrankQiu <iam.frankqiu@gmail.com>
2021-10-27 15:56:56 +0800

    add  a space

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const Avatar = ({ name, picture }: Props) => {
   return (
-    <div className="flex items-center flex-col ">
+    <div className="flex items-center flex-col mt-10">
       <img src={picture} className="w-32 h-32 rounded-full " alt={name} />
       <div className="text-xl font-bold mt-5 text-3xl">{name}</div>
     </div>
